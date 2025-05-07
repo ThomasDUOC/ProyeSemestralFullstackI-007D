1bcda3f2c068cfadb28ea535a9bca716899f1693
CETECOM <CETECOM@AVAAOV02LC0428.duoc.acad>
2025-05-07 17:10:40 -0400

    Spring libraries

---
+++ src/main/java/com/perfulandiagrupo/perfulandiaspa/PerfulandiaspaApplication.java
@@ -0,0 +1,13 @@
+package com.perfulandiagrupo.perfulandiaspa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfulandiaspaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfulandiaspaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perfulandiagrupo/perfulandiaspa/PerfulandiaspaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfulandiagrupo.perfulandiaspa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfulandiaspaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
